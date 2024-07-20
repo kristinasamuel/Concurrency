20514484b089e996444f3f3b7c684d1bc6e3f61c
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2024-07-20 13:35:59 +0500

    Add files via upload
    Concurrency ,  callback function , Promise , async await.

---
+++ async.js
@@ -0,0 +1,108 @@
+// // call back function
+// // we are making cake
+// function mixIngredients(callback: () => void){
+//     console.log("Mixing ingredient...");
+//     setTimeout(()=>{
+//         console.log("Ingredients mixed.");
+//         callback()
+//     },3000)
+// }
+// function bakeCake(callback : () => void){
+//     console.log("Baking cake...");
+//     setTimeout(()=>{
+//         console.log("Cake baked.");
+//         callback()
+//     },4000)
+// }
+// function serveCake(){
+//     console.log("Serving the delicious cake...");
+//     setTimeout(()=>{
+//         console.log("cake served.");
+//     },3000)
+// }
+// mixIngredients(()=>{
+//     bakeCake(()=>{
+//         serveCake();
+//     });
+// });
+// promise
+function mixed_Ingredients() {
+    console.log("Mixing ingredients...");
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Ingredients Mixed");
+        }, 3000);
+    });
+}
+function bake_Cake() {
+    console.log("Baking cake...");
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("cake  Baked !");
+        }, 4000);
+    });
+}
+function serve_Cake() {
+    console.log("Serving the delicious cake...");
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("cake not served !");
+        }, 5000);
+    });
+}
+mixed_Ingredients()
+    .then((value) => {
+    console.log(value);
+    return bake_Cake();
+})
+    .then((value) => {
+    console.log(value);
+    return serve_Cake();
+})
+    .then((value) => {
+    console.log(value);
+})
+    .catch((error) => {
+    console.log(error);
+});
+export {};
+// async await
+// function checkEmail() {
+//   return new Promise((resolve, reject) => {
+//     console.log("Checking Inbox...");
+//     setTimeout(() => {
+//       resolve("inbox checked.");
+//     }, 4000);
+//   });
+// }
+// function readEmail() {
+//   return new Promise((resolve, reject) => {
+//     console.log("Reading emails...");
+//     setTimeout(() => {
+//       resolve("Reading done");
+//     }, 5000);
+//   });
+// }
+// function replyToEmail() {
+//   return new Promise((resolve, reject) => {
+//     console.log("Replying emails...");
+//     setTimeout(() => {
+//       resolve("Email replied.");
+//     }, 6000);
+//   });
+// }
+// async function emailsChecked() {
+//   try {
+//     let result1 = await checkEmail();
+//     console.log(result1);
+//     let result2 = await readEmail();
+//     console.log(result2);
+//     let result3 = await replyToEmail();
+//     console.log(result3);
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     console.log("Email send successfully!!!");
+//   }
+// }
+// emailsChecked();
